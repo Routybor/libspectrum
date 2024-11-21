1bacb090de46eb4efbb6dd059064f40edeb77888
Egor Bondar <e.bondar1@g.nsu.ru>
2024-11-21 12:41:26 +0700

    Remove C++ and change building process

--- src/UsbContext.cpp
+++
@@ -1,7 +0,0 @@
-#ifdef PYSPECTRUM_USE_D2XX
-#include "UsbContextD2XX.cpp"
-#endif // PYSPECTRUM_USE_D2XX
-
-#ifdef PYSPECTRUM_USE_LIBFTDI
-#include "UsbContextLibFTDI.cpp"
-#endif // PYSPECTRUM_USE_LIBFTDI
