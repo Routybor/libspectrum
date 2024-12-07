ec27c31d95b0e7ecfbf3f3c3f044f7ed7422a2be
Egor Bondar <e.bondar1@g.nsu.ru>
2024-12-08 04:22:35 +0700

    Switch for Hatchling build system

---
+++ build.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+import platform
+import shutil
+from pathlib import Path
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        """Called before building the package"""
+        if platform.system() == "Linux":
+            root = Path(self.root)
+            
+            subprocess.check_call(["cmake", "-S", ".", "-B", "build"])
+            subprocess.check_call(["cmake", "--build", "build"])
+            
+            try:
+                (root / "pyspectrum/usb_device.py").unlink()
+                (root / "pyspectrum/usb_context.py").unlink()
+            except FileNotFoundError:
+                pass
+            
+            so_files = list(Path("build").glob("usb_device.*.so"))
+            if so_files:
+                target_dir = root / "pyspectrum"
+                target_dir.mkdir(exist_ok=True)
+                shutil.copy(so_files[0], target_dir)
