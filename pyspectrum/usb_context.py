143d3370ee67cd8d7014d423abcec5e8bda3a155
Egor Bondar <e.bondar1@g.nsu.ru>
2024-12-08 03:54:05 +0700

    Removed linux support from usb_context.py + docstrings

--- pyspectrum/usb_context.py
+++ pyspectrum/usb_context.py
@@ -1,53 +1,80 @@
-import platform
-
-if platform.system() == "Windows" or platform.system() == "Darwin":
-    import ftd2xx as ftd
-else:
-    import pylibftdi as ftdi
+import ftd2xx as ftd
 
 
 class UsbContext:
+    """
+    Класс для работы с устройством FTDI через библиотеку ftd2xx на системе Windows.
+    
+    ### Пример использования:
+    ```
+    context = UsbContext()
+    context.open()
+    context.set_bitmode(0x40, 0x40)
+    context.set_timeouts(5000, 5000)
+    # Ваш код для работы с устройством FTDI...
+    context.close()
+    ```
+    """
     def __init__(self):
         self.device = None
-        self.is_linux = platform.system() != "Windows" and platform.system() != "Darwin"
 
     def open(self):
-        if self.is_linux:
-            self.device = ftdi.Device()
-            self.device.open()
-        else:
-            self.device = ftd.open()
+        """
+        Открывает первое найденное устройство FTDI.
+        
+        :raises RuntimeError: Если устройство не найдено или невозможно его открыть.
+        """
+        self.device = ftd.open()
 
         if not self.device:
             raise RuntimeError("Failed to open device")
 
     def close(self):
+        """
+        Закрывает устройство FTDI.
+        """
         if self.device:
-            if self.is_linux:
-                self.device.close()
-            else:
-                self.device.close()
+            self.device.close()
 
     def set_bitmode(self, mask, enable):
-        if self.is_linux:
-            self.device.ftdi_fn.ftdi_set_bitmode(mask, enable)
-        else:
-            self.device.setBitMode(mask, enable)
+        """
+        Устанавливает режим работы на устройстве FTDI.
+        
+        :param mask: Маска для установки режима работы пинов.
+        :param enable: Режим работы FTDI чипа (0x40 - 245 FIFO Mode)
+        """
+        self.device.setBitMode(mask, enable)
 
     def set_timeouts(self, read_timeout_millis: int, write_timeout_millis: int):
-        if self.is_linux:
-            self.device.ctx.usb_read_timeout = read_timeout_millis
-            self.device.ctx.usb_write_timeout = write_timeout_millis
-        else:
-            self.device.setTimeouts(read_timeout_millis, write_timeout_millis)
+        """
+         Устанавливает таймауты чтения и записи для устройства FTDI.
+
+        :param read_timeout_millis: Таймаут чтения в миллисекундах.
+        :param write_timeout_millis: Таймаут записи в миллисекундах.
+        """
+        self.device.setTimeouts(read_timeout_millis, write_timeout_millis)
 
     def read(self, size) -> bytes:
+        """
+         Читает данные из устройства FTDI.
+        
+        :param size: Количество байтов для чтения.
+        :return: Прочитанные данные в виде байтовой строки.
+        :raises RuntimeError: Если произошла ошибка при чтении данных.
+        """
         try:
             return self.device.read(size)
         except Exception:
             raise RuntimeError("Device read error")
 
     def write(self, data: bytes) -> int:
+        """
+        Записывает данные в устройство FTDI.
+
+        :param data: Данные для записи в виде байтовой строки.
+        :return: Количество записанных байтов.
+        :raises RuntimeError: Если произошла ошибка при записи данных.
+        """
         try:
             return self.device.write(data)
         except Exception:
