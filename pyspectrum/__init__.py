ddce271fb714b0f4fa0ee25a086ea216e1bd93f3
Egor Bondar <e.bondar1@g.nsu.ru>
2024-12-08 04:06:07 +0700

    Added handling of possible import errors on Linux

--- pyspectrum/__init__.py
+++ pyspectrum/__init__.py
@@ -1,3 +1,17 @@
 from .errors import *
 from .data import Data, Spectrum
 from .spectrometer import Spectrometer, FactoryConfig
+
+import platform
+
+if platform.system() == "Windows":
+    from .usb_device import UsbDevice
+    from .usb_context import UsbContext
+else:
+    try:
+        from .usb_device import UsbDevice
+    except ImportError as e:
+        raise ImportError(
+            "Failed to import compiled USB device module."
+            "Make sure you're using the correct version for your platform."
+        ) from e
